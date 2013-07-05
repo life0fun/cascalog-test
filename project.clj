ddd5a623b9bd56b5a4196cbeeca4e096a50e29c1
Haijin Yan <life0fun@gmail.com>
2013-07-04 23:45:35 -0700

    init with readme

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject wordcount "1.0.0-SNAPSHOT"
+  :description "cascalog testing"
+  :dependencies [
+  	[org.clojure/clojure "1.3.0"]
+  	[cascalog "1.10.1"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]])
